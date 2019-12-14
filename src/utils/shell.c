74f6c96a195b80ff382a0a8a5ad99cbc58d494ad
James <jp@James-Mac-6.local>
2019-12-14 11:25:02 -0500

    rip, more pc-ification

--- src/utils/shell.c
+++ src/utils/shell.c
@@ -10,7 +10,7 @@
 #include <dirent.h>      // for listing dirs
 #include <locale.h>      // localization
 #include <stdlib.h>      // some other stuff
-#include <unistd.h>      // for symlinks and shit
+#include <unistd.h>      // for symlinks and s***
 #include <langinfo.h>    // language info
 #include <sys/statvfs.h> // filesystems
 
