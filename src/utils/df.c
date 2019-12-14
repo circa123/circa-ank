fab97b493f60fc1bd825bb3437855e337a1a6ec0
James <jp@James-Mac-6.local>
2019-12-14 11:23:47 -0500

    stylization

--- src/utils/df.c
+++ src/utils/df.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 #include <syscall.h>
-#include <sys/statvfs.h> // filesystems
+#include <sys/statvfs.h>                                                   // filesystems
 
 int main(int argc, char* argv[]) {
     struct statvfs fs;                                                     // filesystem stat struct, for getting free space
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
         printf("error opening path. :(\n");                                // lol rip
         return 1;                                                          // fail
     }
-    
+
     long long free_space;                                                  // long, for free space
     free_space = (fs.f_bavail*4);                                          // set free space in kilobytes
     printf("%s\n", INT_TO_FORMATTED_STRING(free_space*1024));              // printf free space
