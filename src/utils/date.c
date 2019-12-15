2d3d47cc735f5f45ad729a58bcbc91199d59793e
ra1n <ra1n@fake.ema.il>
2019-12-15 11:52:30 -0500

    fixed my lousy programming changed TIME_STRING from char[] to char*

--- src/utils/date.c
+++ src/utils/date.c
@@ -12,11 +12,11 @@ int main() {
     /*
      basic *ss date command
      */
-    time_t    TIME = time(NULL);                                           // time_t, gives us the time
-    struct tm tm = *localtime(&TIME);                                      // get time struct for date command
-    char      TIME_STRING[256];                                            // string holding time
-    setlocale(LC_ALL, "");                                                 // set the locale
-    strftime(TIME_STRING, sizeof(TIME_STRING), nl_langinfo(D_T_FMT), &tm); // format date
-    printf("%s\n", TIME_STRING);                                           // print date string
+    time_t    TIME = time(NULL);                                                       // time_t, gives us the time
+    struct tm tm = *localtime(&TIME);                                                  // get time struct for date command
+    char*      TIME_STRING;                                                            // string holding time
+    setlocale(LC_ALL, "");                                                             // set the locale
+    strftime(TIME_STRING, 256, "%a %d %b %Y %r %Z", &tm);                              // format date
+    printf("%s\n", TIME_STRING);                                                       // print date string
     return 0;
 }
