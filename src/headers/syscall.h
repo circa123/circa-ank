8b6b20da2cb758b3e9be2c58110a25787ae1c7a6
Circa <58899647+circa123@users.noreply.github.com>
2019-12-15 14:40:07 -0500

    Update syscall.h

--- src/headers/syscall.h
+++ src/headers/syscall.h
@@ -7,4 +7,4 @@ int   UNLINK_FILES(const char *F_PATH, const struct stat *STAT_STRUCT, int TYPE,
 int   REMOVE_DIR(char *PATH);                                                                             // remove dir with path
 int   STRING_EQUAL_TO_STRING(char INPUT[], char COMPARE_STRING[], int LEN);                               // compares string to other string
 char* INT_TO_FORMATTED_STRING(long long integer);                                                         // takes integer and puts it to formatted amount of bytes (1KB, 2MB, 4GB, 8TB, etc.)
-int   SPAWN_PTR_WITH_UID_IF_PRIVILEGED(int (*ptr)(int uid), uid);                                         // takes pointer and executes it if uid == ADMIN_ID
+int   SPAWN_PTR_WITH_UID_IF_PRIVILEGED(int (*ptr)(int uid), int uid);                                         // takes pointer and executes it if uid == ADMIN_ID
