e48f3e1457f127228c901c697b92d569cbe62ea1
ra1n <ra1n@fake.ema.il>
2019-12-15 15:57:35 -0500

    comment fixes

--- src/headers/syscall.h
+++ src/headers/syscall.h
@@ -6,4 +6,4 @@
 int   REMOVE_DIR(char *PATH);                                                                             // remove dir with path
 int   STRING_EQUAL_TO_STRING(char INPUT[], char COMPARE_STRING[], int LEN);                               // compares string to other string
 char* INT_TO_FORMATTED_STRING(long long integer);                                                         // takes integer and puts it to formatted amount of bytes (1KB, 2MB, 4GB, 8TB, etc.)
-int   SPAWN_PTR_WITH_UID_IF_PRIVILEGED(int (*ptr)(int uid), int uid);                                         // takes pointer and executes it if uid == ADMIN_ID
+int   SPAWN_PTR_WITH_UID_IF_PRIVILEGED(int (*ptr)(int uid), int uid);                                     // takes pointer and executes it if uid == ADMIN_ID
