127c7502702bda5162cb749b3d2d9d569da952cb
James <ra1n@fake.ema.il>
2019-12-15 09:04:36 -0500

    basic stuff, just removed a space before a newline :)

--- src/ank/init.c
+++ src/ank/init.c
@@ -11,7 +11,7 @@ int main(){
     /*
      stole fork + exec code from shell.c
      */
-    printf("{ank} init\nTHIS PROGRAM COMES WITH ABSOLUTELY NO WARRANTY, SEE LICENSE TEXT FOR DETAILS. \n");
+    printf("{ank} init\nTHIS PROGRAM COMES WITH ABSOLUTELY NO WARRANTY, SEE LICENSE TEXT FOR DETAILS.\n");
     int CHILD = 0;
     int status;                                                            // for waitpid
     int pid = fork();                                                      // fork
