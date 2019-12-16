6e0ca71cdb209aa7cc1ca9444f8d9852f912fa12
ra1n <ra1n@fake.ema.il>
2019-12-16 09:50:15 -0500

    remove useless

--- src/utils/dirname.c
+++ src/utils/dirname.c
@@ -8,12 +8,11 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    char* token;
     if (argc == 1) {
         printf("dirname: usage: dirname [PATH]\n");                        // no args specified.
         return 1;
     }
-    
+
     printf("%s\n", dirname(argv[1]));                                      // run dirname syscall on argv[1]
     return 0;
 }
