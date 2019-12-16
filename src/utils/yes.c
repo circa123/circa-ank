05a4cf846cc727986da8d96733928442cd6130e3
ra1n <ra1n@fake.ema.il>
2019-12-16 09:49:54 -0500

    CHANGES added int argc, and char* argv[] to yes doesn't really matter, but whatever

--- src/utils/yes.c
+++ src/utils/yes.c
@@ -5,10 +5,10 @@
 
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     while (1) {
         printf("yes\n");                                                   // i f***ing hate yes
     }
-    
+
     return 0;                                                              // success
 }
