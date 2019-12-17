2773cd1b0dc9aa9f75ae2c498daaf509594f15a0
Circa <ra1n@fake.ema.il>
2019-12-17 11:22:06 -0500

    because posix

--- src/utils/chmod.c
+++ src/utils/chmod.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[]) {
      chmod, circa edition
      */
     if (argc < 3) {
-        printf("chmod: file mode\ne.g. chmod example 777\n"); // if not enough arguments
+        printf("chmod: file mode\ne.g. chmod 777 example\n"); // if not enough arguments
         return argc;                                          // return argument count
     }
 
     else {
-        return chmod(argv[1], strtol(argv[2], NULL, 8));      // otherwise, chmod first argument [argv[1]], with permissions [argv[2]]
+        return chmod(argv[2], strtol(argv[1], NULL, 8));      // otherwise, chmod first argument [argv[1]], with permissions [argv[2]]
     };
 }
