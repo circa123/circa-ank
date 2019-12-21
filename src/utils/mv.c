2f048c6d5b16b19f6bb99650b9170efa1f1556d7
ra1n <ra1n@fake.ema.il>
2019-12-20 20:04:07 -0500

    lol syscalls i just want to point out that I am hallucinating from exaughstion at the moment, maybe i shouldn't program?

--- src/utils/mv.c
+++ src/utils/mv.c
@@ -13,48 +13,5 @@ int main(int argc, char* argv[]) {
         printf("mv: usage: mv [FILE] [NEW_NAME]\ne.g. mv file new_file");
     }
 
-    FILE *fptr;                                                            // file pointer
-    FILE *sndptr;                                                          // second file pointer, for writing
-    int ctr;                                                               // counter for reading
-    char chr;                                                              // char for reading
-    char FILE_READ[1024];                                                  // read buffer
-
-    /*
-     debugging
-    for (int i = 0; i < strlen(args[2]); i++) {
-        printf("%x", args[2][i]);
-    };
-    printf("\n");
-     */
-
-    fptr   = fopen(argv[1], "r");                                          // open file, for reading
-    sndptr = fopen(argv[2], "w");                                          // open file, for writing
-
-    if (fptr == NULL) {
-        printf("error opening file 1 :(\n");                               // lol rip
-        return 1;                                                          // fail
-    }
-
-    if (sndptr == NULL) {
-        printf("error opening file 2 :(\n");                               // lol rip
-        return 1;                                                          // fail
-    }
-
-    int size;                                                              // size of file
-    fseek(fptr, 0, SEEK_END);                                              // go to end of file
-    size = ftell(fptr);                                                    // check where we are and save
-    fseek(fptr, 0, 0);                                                     // go back to start
-
-    chr = fgetc(fptr);                                                     // get next character
-    int cnt = 0;                                                           // counter for file read
-    while (cnt < size) {                                                   // read until file over
-        fputc(chr, sndptr);                                                // write file
-        chr = fgetc(fptr);                                                 // get next char
-        cnt++;                                                             // up counter
-    }
-
-    fclose(fptr);                                                          // close file
-    fclose(sndptr);                                                        // close writing file
-    remove(argv[1]);                                                       // bye
-    return 0;                                                              // success
+    return rename(argv[1], argv[2]);
 }
